f01688b44ddd60fe257212f82ca88f03c839e853
gummybuns <gummybuns@protonmail.com>
2025-02-22 20:45:20 +0000

    trying to make audio_ctrl.h match styles

--- audio_ctrl.h
+++ audio_ctrl.h
@@ -16,33 +16,33 @@
 
 #define ENC_OPTION_OFFSET 97
 
-typedef struct audioconfig {
-	u_int sample_rate;
-	u_int precision;
-	u_int channels;
-	u_int encoding;
-    u_int buffer_size;
+typedef struct audio_config_t {
+	unsigned int buffer_size;
+	unsigned int channels;
+	unsigned int encoding;
+	unsigned int precision;
+	unsigned int sample_rate;
 } audio_config_t;
 
-typedef struct encoding_options {
+typedef struct encoding_options_t {
 	int total;
 	audio_encoding_t *encodings;
 } encoding_options_t;
 
-typedef struct audio_controller {
-	char *path;
+typedef struct audio_ctrl_t {
 	int fd;
 	int mode;
-	audio_config_t config;
 	audio_config_t hw_info;
+	audio_config_t config;
 	encoding_options_t encoding_options;
+	char *path;
 } audio_ctrl_t;
 
-int build_audio_ctrl(audio_ctrl_t *ctrl, char *path, int mode);
+int build_audio_ctrl(audio_ctrl_t * ctrl, char *path, int mode);
 void print_ctrl(audio_ctrl_t ctrl);
-void print_encodings(audio_ctrl_t *ctrl, int offset);
+void print_encodings(audio_ctrl_t * ctrl, int offset);
 int get_max_encoding(audio_ctrl_t ctrl);
-int set_encoding(audio_ctrl_t *ctrl, audio_encoding_t encoding);
+int set_encoding(audio_ctrl_t * ctrl, audio_encoding_t encoding);
 const char *get_encoding_name(u_int encoding);
 const char *get_mode(audio_ctrl_t ctrl);
 
